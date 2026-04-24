80c8c44d75208fa7a342b6b7b7968b6103904ffe
Naim <naimislmmd1020@gmail.com>
2026-04-25 01:00:02 +0600

    add login and signup frontend with api integration

---
+++ frontend/login-signup/src/pages/login.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("token", res.data.token);
+
+      alert("Login successful");
+     
+      setEmail("");
+      setPassword("");
+
+    } 
+    catch (err) {
+      alert("Invalid email or password");
+      console.error(err.response?.data);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-indigo-600">
+      <form
+        onSubmit={handleLogin}
+        className="bg-white p-8 rounded-2xl shadow-lg w-80"
+      >
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6 font-serif">
+          Login
+        </h2>
+
+        <input
+          type="email"
+          value={email}
+          placeholder="Enter your email"
+          className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          value={password}
+          placeholder="Enter your password"
+          className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-indigo-500 text-white py-2 rounded-lg hover:bg-indigo-600 transition duration-300"
+        >
+          Login
+        </button>
+
+        <p className="text-sm text-center mt-4 text-gray-600">
+          Don’t have an account?{" "}
+          <span
+            onClick={() => navigate("/signup")}
+            className="text-indigo-500 font-semibold cursor-pointer hover:underline"
+          >
+            SignUp
+          </span>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
---
+++ frontend/login-signup/src/pages/signUp.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+function Signup() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:5000/api/auth/register", {
+        email,
+        password,
+      });
+
+      console.log(res.data);
+      alert("Signup successful");
+      setEmail("");
+      setPassword("");
+
+    } catch (err) {
+      alert("Error signing up");
+      console.error(err.response?.data);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 to-purple-600">
+      <form
+        onSubmit={handleSignup}
+        className="bg-white p-8 rounded-2xl shadow-lg w-80"
+      >
+        <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
+          Create Account
+        </h2>
+
+        <input
+          type="email"
+          value={email}
+          placeholder="Enter your email"
+          className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          value={password}
+          placeholder="Enter your password"
+          className="w-full px-4 py-2 mb-4 border rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400"
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button
+          type="submit"
+          className="w-full bg-indigo-500 text-white py-2 rounded-lg hover:bg-indigo-600 transition duration-300"
+        >
+          Sign Up
+        </button>
+
+        <p className="text-sm text-center mt-4 text-gray-600">
+          Already have an account?{" "}
+          <span className="text-indigo-500 font-semibold cursor-pointer hover:underline" onClick={()=>navigate("/login")}>
+            Login
+         
+          </span>
+          
+        </p>
+        
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
